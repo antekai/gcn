4f9d7dd436a2410957b5563a9ade7bac1f987b51
Andreas Teneketzoglou <antekai00@gmail.com>
2018-01-22 01:47:35 +0100

    setup page creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,55 +1,53 @@
-const _ = require(`lodash`)
-const Promise = require(`bluebird`)
-const path = require(`path`)
-const slash = require(`slash`)
-
-// We'll need something for creating the slugs
-const slugify = require(`slugify`)
-const slugifyOptions = {
-  replacement: '-',
-  remove: /[$*_+~.()'"!\-:@]/g,
-  lower: true
-}
-
-// We'll use Gatsby's createPage API for generating the pages
-exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
-  return new Promise((resolve, reject) => {
-    // This is where we'll do most of our work
-    graphql(
+ const _ = require(`lodash`)
+ const Promise = require(`bluebird`)
+ const path = require(`path`)
+ const slash = require(`slash`)
+ const slugify = require(`slugify`)
+ 
+ const slugifyOptions = {
+    replacement: '-',
+    remove: /[$*_+~.()'"!\-:@]/g,
+    lower: true
+  }
+ 
+ 
+ exports.createPages = ({ graphql, boundActionCreators }) => {
+    const { createPage } = boundActionCreators
+    return new Promise((resolve, reject) => {
+      graphql(
         `
-            {
-                allContentfulPage(limit: 1000) {
-                    edges {
-                        node {
-                            id
-                            title
-                        }
-                    }
+          {
+            allContentfulPage(limit: 1000) {
+              edges {
+                node {
+                  id
+                  title
                 }
+              }
             }
+          }
         `
-    )
-    .then(result => {
-        if (result.errors) {
+      )
+        .then(result => {
+          if (result.errors) {
             reject(result.errors)
-        }
-
-        // We'll do the actual page creation here
-        const pageTemplate = path.resolve(`./src/templates/subpage.js`)
-        _.each(result.data.allContentfulPage.edges, edge => {
-            // Here's the beef, seitan, or whatever rocks your boat:
+          }
+ 
+          // Create Sub Pages
+          const pageTemplate = path.resolve(`./src/templates/subpage.js`)
+          _.each(result.data.allContentfulPage.edges, edge => {
+ 
+            // Here's the beef
             createPage({
-                path: `/pages/${slugify(edge.node.title, slugifyOptions)}/`,
-                component: slash(pageTemplate),
-                context: {
-                    id: edge.node.id
-                },
+              path: `/pages/${slugify(edge.node.title, slugifyOptions)}/`,
+              component: slash(pageTemplate),
+              context: {
+                id: edge.node.id
+              },
             })
+ 
+          })
+          resolve()
         })
-        
-        resolve() // Resolve the promise
     })
-})
-  })
-}
+  }
