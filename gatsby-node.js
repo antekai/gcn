313672930d8824e6c2cc4684840cfa6e673d90d4
Andreas Teneketzoglou <antekai00@gmail.com>
2018-01-24 21:00:12 +0100

    create blog posts - handler

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,53 +1,90 @@
- const _ = require(`lodash`)
- const Promise = require(`bluebird`)
- const path = require(`path`)
- const slash = require(`slash`)
- const slugify = require(`slugify`)
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+const _ = require(`lodash`)
+const Promise = require(`bluebird`)
+const path = require(`path`)
+const slash = require(`slash`)
+const slugify = require(`slugify`)
+
+const slugifyOptions = {
+   replacement: '-',
+   remove: /[$*_+~.()'"!\-:@]/g,
+   lower: true
+ }
+
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+   const { createPage } = boundActionCreators
+   return new Promise((resolve, reject) => {
+     graphql(
+       `
+         {
+           allContentfulPage(limit: 1000) {
+             edges {
+               node {
+                 id
+                 title
+               }
+             }
+           }
+         }
+       `
+     )
+       .then(result => {
+         if (result.errors) {
+           reject(result.errors)
+         }
  
- const slugifyOptions = {
-    replacement: '-',
-    remove: /[$*_+~.()'"!\-:@]/g,
-    lower: true
-  }
- 
- 
- exports.createPages = ({ graphql, boundActionCreators }) => {
-    const { createPage } = boundActionCreators
-    return new Promise((resolve, reject) => {
-      graphql(
-        `
-          {
-            allContentfulPage(limit: 1000) {
-              edges {
-                node {
-                  id
-                  title
-                }
-              }
-            }
-          }
-        `
-      )
-        .then(result => {
-          if (result.errors) {
-            reject(result.errors)
-          }
- 
-          // Create Sub Pages
-          const pageTemplate = path.resolve(`./src/templates/subpage.js`)
-          _.each(result.data.allContentfulPage.edges, edge => {
- 
-            // Here's the beef
-            createPage({
-              path: `/pages/${slugify(edge.node.title, slugifyOptions)}/`,
-              component: slash(pageTemplate),
-              context: {
-                id: edge.node.id
-              },
-            })
- 
-          })
-          resolve()
-        })
-    })
-  }
+         // Create Sub Pages
+         const pageTemplate = path.resolve(`./src/templates/subpage.js`)
+         _.each(result.data.allContentfulPage.edges, edge => {
+           createPage({
+             path: `/pages/${slugify(edge.node.title, slugifyOptions)}/`,
+             component: slash(pageTemplate),
+             context: {
+               id: edge.node.id
+             },
+           })
+         })
+         resolve()
+       })
+       .then(() => {
+           graphql(
+               `
+                 {
+                   allContentfulBlogPost(limit: 1000) {
+                     edges {
+                       node {
+                         id
+                         title
+                       }
+                     }
+                   }
+                 }
+               `
+           )
+           .then(result => {
+               if (result.errors) {
+               reject(result.errors)
+               }
+       
+               // Create Blog Posts
+               const postTemplate = path.resolve(`./src/templates/blogpost.js`)
+               _.each(result.data.allContentfulBlogPost.edges, edge => {
+               createPage({
+                   path: `/posts/${slugify(edge.node.title, slugifyOptions)}/`,
+                   component: slash(postTemplate),
+                   context: {
+                   id: edge.node.id
+                   },
+               })
+               })
+               resolve()
+           })
+       })
+   })
+ }
