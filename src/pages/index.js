d58bf259c07181ba5daa1e6ba1bb72f92df33bb5
Andreas Teneketzoglou <antekai00@gmail.com>
2018-01-24 21:05:38 +0100

    add blog list to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import Gallery from '../templates/Gallery'
+import Gallery from '../templates/gallery'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <h2>Here's my gallery:</h2>
-    <Gallery /> 
-    <h2>Here are my subpages:</h2>
-    <ul>
-      <li><Link to="/subpage/">A subpage</Link></li>
-    </ul>    
-    <h2>Here are my posts:</h2>
-    <ul>
-      <li><Link to="/blogpost/">A post</Link></li>
-    </ul>
-  </div>
-)
+class IndexPage extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Hi people</h1>
+    
+        <h2>Here's my gallery:</h2>
+        <Gallery />
+        
+        <h2>Here are my subpages:</h2>
+        <ul>
+          <li><Link to="/subpage/">A subpage</Link></li>
+        </ul>
+        
+        <h2>Here are my posts:</h2>
+        <ul>
+          <li><Link to="/blogpost/">A post</Link></li>
+        </ul>
+      </div>
+    )    
+  }
+}
 
 export default IndexPage
